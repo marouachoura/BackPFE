03603da10c37e112d4819f1fa88d4750134c735a
marouachoura <marouachoura20@gmail.com>
2022-05-30 13:02:19 +0200

    changement dans le couche controller

--- pfe/src/main/java/com/example/demo/contoller/CoucheController.java
+++ pfe/src/main/java/com/example/demo/contoller/CoucheController.java
@@ -43,12 +43,14 @@ public class CoucheController {
 		return iService.findSiteEmploye(id);	
 	
 	}*/
+        @PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/employes/formations/{id}", method = RequestMethod.GET)
 	public List <Formation> findFormationsEmploye(@PathVariable Long id) {
 		return iService.findFormationsdEmploye(id);	
 	
 	}
-	
+
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PostMapping(value = "/employes/add")
 
 	public Employe addEmploye(@RequestBody Employe e)
@@ -57,27 +59,33 @@ public class CoucheController {
 		return iService.addEmploye(e);
 		
 	}
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/employes/find/{id}", method = RequestMethod.GET)
 	public Employe findEmployeByid(@PathVariable Long id) {
 		return iService.findEmploye(id);	
 	
 	}
+
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@DeleteMapping(value="/employes/delete/{id}")
 	public void deleteEmploye(@PathVariable Long id)
 	{
 		iService.deleteEmploye(id);
 	}
-
+	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/formations", method = RequestMethod.GET)
 	public List<Formation> findAllFormations() {
 		return iService.findAllFormations();
 	}
 	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/formations/find/{id}", method = RequestMethod.GET)
 	public Formation findFormation(@PathVariable Long id) {
 		return iService.findFormation(id);	
 	}
 	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PostMapping(value = "/formations/add")
 
 	public Formation addFormation(@RequestBody Formation f)
@@ -85,11 +93,14 @@ public class CoucheController {
 		return iService.addFormation(f);
 	}
 	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@DeleteMapping(value="/formations/delete/{id}")
 	public void deleteFormation(@PathVariable Long id)
 	{
 		iService.deleteFormation(id);
 	}
+	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PutMapping(value="/formation/update/{id}")
 	public Formation updateFormation(@PathVariable Long id, @RequestBody Formation f)
 	{
@@ -97,29 +108,36 @@ public class CoucheController {
 		return iService.updateFormation(f);
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/sites", method = RequestMethod.GET)
 	public List<Site> findAllSites() {
 		return iService.findAllSites();
 
 	}
-
+	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@RequestMapping(value = "/sites/find/{id}", method = RequestMethod.GET)
 	public Site findSitesById(@PathVariable Long id) {
 		return iService.findSite(id);
 
 	}
+	
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@DeleteMapping(value="/sites/delete/{id}")
 	public void deleteSite(@PathVariable Long id)
 	{
 		iService.deleteSite(id);
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PostMapping(value = "/sites/add")
 
 	public Site addSite(@RequestBody Site site)
 	{
 		return iService.addSite(site);
 	}
+
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PutMapping(value="/sites/update/{id}")
 	public Site updateSite(@PathVariable Long id, @RequestBody Site site)
 	{
@@ -127,6 +145,8 @@ public class CoucheController {
 		return iService.updateSite(site);
 	}
 	
+
+	@PreAuthorize("hasRole('ROLE_USER' ) or hasRole('ROLE_ADMIN') ")
 	@PutMapping(value = "/employes/{idemp}/formations/{idformation}")
 	public void affecter(@PathVariable(value = "idemp")  Long idemp, @PathVariable(value = "idformation")  Long idformation) {
 		 iService.affectuerFormationToEmploye(idemp, idformation);
